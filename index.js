402d63662ec0791a03385004486a1c32041f820c
Thabang Rakgoropo <rakgoropothabang@gmail.com>
2023-09-29 21:23:43 +0200

    Completed the Variables section

---
+++ index.js
@@ -0,0 +1,12 @@
+let userName="Thabang";
+let age=24
+let student =true;
+
+
+document.getElementById("p1").innerHTML="My name "+userName;
+document.getElementById("p2").innerHTML="Age "+ age +" Years old"
+console.log(userName);
+console.log(age);
+
+//Arithmetic Expressions
+
